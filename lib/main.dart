57cb55d7f5943bead8d3b724514a8701eab2d577
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-04-20 17:04:00 +0700

    Add floating btn and create state

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,33 +16,42 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int number = 0; // สร้าง State
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("ToDo List"),
+      appBar: AppBar(
+        title: const Text("ToDo List"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "คลิกเพื่อเพิ่มจำนวนตัวเลข",
+              style: TextStyle(fontSize: 24, color: Colors.red),
+            ),
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 60),
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://www.wfla.com/wp-content/uploads/sites/71/2023/05/GettyImages-1389862392.jpg?w=2560&h=1440&crop=1"),
+            ),
+          ],
         ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Hello Dart",
-                style: TextStyle(fontSize: 24, color: Colors.red),
-              ),
-              Image(
-                image: NetworkImage(
-                    "https://www.wfla.com/wp-content/uploads/sites/71/2023/05/GettyImages-1389862392.jpg?w=2560&h=1440&crop=1"),
-              ),
-            ],
-          ),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
