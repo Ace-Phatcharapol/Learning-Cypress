288fe9970e5490c24a3853f22f6674abb8853109
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-05-08 23:36:21 +0700

    Add base colors file

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color cRed = Color.fromARGB(255, 255, 17, 0);
+const Color cBlue = Color.fromARGB(255, 0, 68, 255);
+const Color cBlack = Color.fromARGB(255, 0, 0, 0);
+const Color cGrey = Color.fromARGB(255, 66, 66, 66);
+const Color cWhite = Color.fromARGB(255, 255, 255, 255);
