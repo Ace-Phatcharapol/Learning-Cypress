7291476e691718dfca169f28f9b996ecf466963b
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-05-09 17:13:21 +0700

    Creating Search Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:myflutter/constants/colors.dart';
 
 class Home extends StatelessWidget {
@@ -7,34 +8,66 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: cWhite,
+        backgroundColor: cWhite,
         appBar: _buildAppBar(),
         body: Container(
-          child: Text('ToDo List'),
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              searchBox(),
+            ],
+          ),
         ));
   }
 
-    AppBar _buildAppBar() {
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+          decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search,
+          color: cBlack,
+          size: 20,
+        ),
+        prefixIconConstraints: BoxConstraints(
+          maxHeight: 20,
+          minWidth: 25,
+        ),
+        border: InputBorder.none,
+        hintText: 'Search',
+        hintStyle: TextStyle(color: cGrey),
+      )),
+    );
+  }
+
+  AppBar _buildAppBar() {
     return AppBar(
-        backgroundColor: cWhite,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(Icons.menu,
+      backgroundColor: cWhite,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
             color: cBlack,
             size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/Fat Cat.jpg'),
             ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset('assets/images/Fat Cat.jpg'),
-              ),
-            ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
